46bd860491a1f07a9f83471ba20ae7dc30c38b34
ludeeus <joasoe@gmail.com>
2018-07-09 17:38:28 +0200

    3.0.0 - Cleanup

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # custom_component to update Cloudflare records
   
-[![Version](https://img.shields.io/badge/version-2.0.0-green.svg?style=for-the-badge)](#) [![mantained](https://img.shields.io/maintenance/yes/2018.svg?style=for-the-badge)](#) [![maintainer](https://img.shields.io/badge/maintainer-Joakim%20Sørensen%20%40ludeeus-blue.svg?style=for-the-badge)](#)   
+[![Version](https://img.shields.io/badge/version-2.0.1-green.svg?style=for-the-badge)](#) [![mantained](https://img.shields.io/maintenance/yes/2018.svg?style=for-the-badge)](#) [![maintainer](https://img.shields.io/badge/maintainer-Joakim%20Sørensen%20%40ludeeus-blue.svg?style=for-the-badge)](#)  
 A platform which allows you to update the IP adderesses of your Cloudflare DNS records.
   
 To get started put `/custom_components/cloudflare.py`  
 here: `<config directory>/custom_components/cloudflare.py`  
   
 **Example configuration.yaml:**
+
 ```yaml
 cloudflare:
   email: 'user@example.com'
-  key: 'c2547eb745079dac9320b638f5e225cf483cc5cfdda41'
+  api_key: 'c2547eb745079dac9320b638f5e225cf483cc5cfdda41'
   zone: 'example.com'
   records:
     - 'bin'
     - 'www'
 ```
+
 **Configuration variables:**  
   
 key | description  
--- custom_components/cloudflare.py
+++ custom_components/cloudflare.py
@@ -4,77 +4,78 @@ For more details about this component, please refer to the documentation at
 https://github.com/HalfDecent/HA-Custom_components/cloudflare
 """
 import logging
-import voluptuous as vol
 from datetime import timedelta
-from homeassistant.helpers.entity import Entity
+import voluptuous as vol
+
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import track_time_interval
+from homeassistant.const import (CONF_API_KEY, CONF_EMAIL, CONF_ZONE)
 
 REQUIREMENTS = ['pycfdns==0.0.1']
 
-DOMAIN = 'cloudflare'
+__version__ = '3.0.0'
 
-CONF_EMAIL = 'email'
-CONF_KEY = 'key'
-CONF_ZONE = 'zone'
+DOMAIN = 'cloudflare'
 CONF_RECORDS = 'records'
 
 INTERVAL = timedelta(minutes=60)
 TIMEOUT = 10
-SERVICE_UPDATE = 'update_records'
-COMPONENT_NAME = 'cloudflare'
-COMPONENT_VERSION = '2.0.0'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_EMAIL): cv.string,
-        vol.Required(CONF_KEY): cv.string,
+        vol.Required(CONF_API_KEY): cv.string,
         vol.Required(CONF_ZONE): cv.string,
-        vol.Optional(CONF_RECORDS, default='None'): 
+        vol.Optional(CONF_RECORDS, default='None'):
             vol.All(cv.ensure_list, [cv.string]),
-})
+    })
 }, extra=vol.ALLOW_EXTRA)
 
 _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
     """Set up the component."""
+    _LOGGER.info('version %s is starting, if you have ANY issues with this, please report them here: https://github.com/custom-components/%s', __version__, __name__.split('.')[1])
     from pycfdns import CloudflareUpdater
     cfupdate = CloudflareUpdater()
     email = config[DOMAIN][CONF_EMAIL]
-    key = config[DOMAIN][CONF_KEY]
+    key = config[DOMAIN][CONF_API_KEY]
     zone = config[DOMAIN][CONF_ZONE]
-    records = config[DOMAIN][CONF_RECORDS]      
+    records = config[DOMAIN][CONF_RECORDS]
 
-    def update_domain_interval(now):
+    def update_records_interval(now):
         """Set up recuring update."""
         _update_cloudflare(cfupdate, email, key, zone, records)
 
-    def update_domain_service(call):
+    def update_records_service(now):
         """Set up service for manual trigger."""
         _update_cloudflare(cfupdate, email, key, zone, records)
 
-    track_time_interval(hass, update_domain_interval, INTERVAL)
+    track_time_interval(hass, update_records_interval, INTERVAL)
     hass.services.register(
-        DOMAIN, SERVICE_UPDATE, update_domain_service)
+        DOMAIN, 'update_records', update_records_service)
     return True
 
 def _update_cloudflare(cfupdate, email, key, zone, records):
     """Update DNS records for a given zone."""
     _LOGGER.debug('Starting update for zone %s.', zone)
-    #Set headers:
-    headers = cfupdate.set_header(email,key)
-    _LOGGER.debug('header data defined as: %s.', headers)
-    #Get zoneID:
-    zoneID = cfupdate.get_zoneID(headers, zone)
-    _LOGGER.debug('zoneID is set to: %s.', zoneID)
+
+    #Set request headers:
+    headers = cfupdate.set_header(email, key)
+    _LOGGER.debug('Header data defined as: %s.', headers)
+
+    #Get zone ID:
+    zoneid = cfupdate.get_zoneID(headers, zone)
+    _LOGGER.debug('Zone ID is set to: %s.', zoneid)
+
     #Get records to update:
-    updateRecords = cfupdate.get_recordInfo(headers, zoneID, zone, records)
-    _LOGGER.debug('records: %s.', updateRecords)
+    update_records = cfupdate.get_recordInfo(headers, zoneid, zone, records)
+    _LOGGER.debug('Records: %s.', update_records)
+
     #Update records:
-    result = cfupdate.update_records(headers, zoneID, updateRecords)
+    result = cfupdate.update_records(headers, zoneid, update_records)
     _LOGGER.debug('Update for zone %s is complete.', zone)
-    if result == True:
-        result = "Update complete."
-    _LOGGER.info(result)
+
+    if result != True:
+        _LOGGER.warning(result)
     return True
